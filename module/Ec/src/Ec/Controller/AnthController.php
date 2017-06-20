1097a2cbedb85df929f828ec3a1a60ce7977db6a
AD\onoe-azusa <onoe-azusa@imjp.co.jp>
2017-06-20 18:42:33 +0900

    ログイン機能作成　URLその1　

---
+++ module/Ec/src/Ec/Controller/AnthController.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace Ec\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+/**
+ * Description of AnthController
+ *
+ * @author onoe-azusa
+ */
+class AnthController extends AbstractActionController
+{
+    //put your code here
+    public function signupAction()
+    {
+
+    }
+
+    public function loginAction()
+    {
+
+    }    
+    
+    
+}
