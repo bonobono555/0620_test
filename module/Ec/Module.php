06e713a93d054bd6b92995562b307ef9c175b187
AD\onoe-azusa <onoe-azusa@imjp.co.jp>
2017-06-20 19:14:46 +0900

    module.php　テーブルの増やし方分からない

--- module/Ec/Module.php
+++ module/Ec/Module.php
@@ -18,6 +18,7 @@ use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\TableGateway\TableGateway;
 use Ec\Model\UserTable;
 use Ec\Model\User;
+use Ec\Model\LoginTable; //ログインテーブル 
 //-----------------------
 
 use Zend\Db\Adapter\Adapter;    // ←追加
@@ -86,4 +87,8 @@ class Module
         );
     }
     // DB接続のため追加----
+    
+    
+    // ログインテーブルのDBの追加はどうすればいいか？
+
 }
