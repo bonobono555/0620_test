5d8bcc6c9ca70dafa25a1a9708346457f9833014
AD\onoe-azusa <onoe-azusa@imjp.co.jp>
2017-06-22 18:19:01 +0900

    ログイン項目　LoginForm

---
+++ module/Ec/src/Ec/Form/LoginForm.php
@@ -0,0 +1,44 @@
+<?php
+namespace Ec\Form;
+
+use Zend\Form\Form;
+use Zend\Form\Element;
+
+/**
+ * Description of LoginForm
+ *
+ * @author onoe-azusa
+ */
+class LoginForm extends Form
+{
+     public function __construct()
+    {  
+        parent::__construct();
+        $this->add(array(
+            'name' => 'email',
+            'attributes' => array(
+                'type' => 'text',
+            ),
+            'options' => array(
+                'label' => 'email',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'password',
+            'attributes' => array(
+                'type' => 'password',
+            ),
+            'options' => array(
+                'label' => 'password',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'submit',
+            'attributes' => array(
+                'type' => 'submit',
+                'value' => 'login',
+            ),
+        ));
+        $this->add(new Element\Csrf('csrf'));
+    }
+}
