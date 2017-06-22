e9b8fabbdb96267cd6a4926008b1f94c022a0fb6
AD\onoe-azusa <onoe-azusa@imjp.co.jp>
2017-06-22 18:19:53 +0900

    ログイン機能のloginAction getDbAdapter　を追加

--- module/Ec/src/Ec/Controller/AuthController.php
+++ module/Ec/src/Ec/Controller/AuthController.php
@@ -11,6 +11,9 @@ namespace Ec\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Ec\Form\RegisterForm;
 use Ec\Model\People;
+use Ec\Form\LoginForm;
+use Zend\Authentication\Adapter\DbTable as AuthAdapter;
+use Zend\Authentication\AuthenticationService;
 
 /**
  * ログイン機能
@@ -20,24 +23,32 @@ use Ec\Model\People;
  */
 class AuthController extends AbstractActionController
 {
+    // メンバ変数
     protected $peopleTable;
+    protected $dbAdapter;
 
     public function signupAction()
     {
+        // RefisterFormを呼び出す
         $form = new RegisterForm();
+        // リクエストメソッド
         $request = $this->getRequest();
 
+        // ポストされている場合
         if ($request->isPost()) {
             $people = new People();
+            // getInputFilterでフィルターを実行（バリデーション的な感じ？）
             $form->setInputFilter($people->getInputFilter());
             $form->setData($request->getPost());
 
             if ($form->isValid()) {
-                $egist->exchangeArray($form->getData());
+                $people->exchangeArray($form->getData());
+                // DB登録
                 $this->getPeopleTable()->registerPeople($people);
                 exit('登録完了！以下の処理保留');
             }
         }
+        // viewにformの値を渡す
         return array('form' => $form);
     }
 
@@ -48,5 +59,49 @@ class AuthController extends AbstractActionController
             $this->peopleTable = $sm->get('Ec\Model\PeopleTable');
         }
         return $this->peopleTable;
+    }
+    
+    /*
+     * ログインアクション
+     */
+    public function loginAction()
+    {
+        $form = new LoginForm();
+        $request = $this->getRequest();
+
+        if ($request->isPost()) {
+            $people = new People();
+            $form->setInputFilter($people->getInputFilter());
+            $form->setData($request->getPost());
+            
+            if ($form->isValid()) {
+                $dbAdapter = $this->getDbAdapter();
+                $authAdapter = new AuthAdapter($dbAdapter);
+
+                $peopleData = $form->getData();
+                // 対応するテーブル名、ユニークなカラム名、パスワードカラム名
+                $authAdapter->setTableName('people')->setIdentityColumn('email')->setCredentialColumn('password');
+                // 受け取ったデータ
+                $authAdapter->setIdentity($peopleData['email'])->setCredential($peopleData['password']);
+                // 必須ではないがAuthenticationServiceを利用
+                $auth = new AuthenticationService();
+                $result = $auth->authenticate($authAdapter);
+                exit(var_dump($result->isValid()));
+            }
+        }
+                // viewにformの値を渡す
+        return array('form' => $form);
+    }
+
+    /*
+     * データベースと接続するアダプターを取得
+     */
+    public function getDbAdapter()
+    {
+        if (!$this->dbAdapter) {
+            $sm = $this->getServiceLocator();
+            $this->dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+        }
+        return $this->dbAdapter;
     }
 }
