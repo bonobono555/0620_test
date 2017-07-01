9540e50f4a0e626ec72ae3ad7e896026a82cba2d
onoeadusa <onoe.adusa@gmail.com>
2017-07-01 17:29:05 +0900

    呼んだメンバ変数が間違いだったため修正

--- module/Ec/src/Ec/Model/User.php
+++ module/Ec/src/Ec/Model/User.php
@@ -6,8 +6,9 @@ use Zend\InputFilter\Factory as InputFactory;
 use Zend\InputFilter\InputFilter;
 use Zend\InputFilter\InputFilterAwareInterface;
 use Zend\InputFilter\InputFilterInterface;
-//implements InputFilterAwareInterface
-class User 
+
+// 1つのオブジェクトが１つのレコード内容と一致するユーザー情報を保持するモデルクラス
+class User implements InputFilterAwareInterface
 {
     public $id;
     public $name;
@@ -17,24 +18,29 @@ class User
     public $url;
     
     protected $inputFilter;
-        
+    
+    // メンバ変数を初期化する処理
     public function exchangeArray($data)
     {
+        // 引数に配列を想定し、目的とする添え字に対して値があれば、値をメンバに格納。
+        // なければデフォルト値をメンバへ格納しています。
+        // デフォルト値はデータベースの絡むの型と揃える
         $this->id = (isset($data['id'])) ? $data['id'] : 0;
         $this->name = (isset($data['name'])) ? $data['name'] : '';
         $this->email = (isset($data['email'])) ? $data['email'] : '';
-        $this->email = (isset($data['password'])) ? $data['password'] : '';
-        $this->email = (isset($data['comment'])) ? $data['comment'] : '';
-        $this->email = (isset($data['url'])) ? $data['url'] : '';
+        $this->password = (isset($data['password'])) ? $data['password'] : '';
+        $this->comment = (isset($data['comment'])) ? $data['comment'] : '';
+        $this->url = (isset($data['url'])) ? $data['url'] : '';
 
     }
     
-    
+    // implementsすることでInputFilterAwareInterface内にあるsetInputFilterの実装が義務となる
     public function setInputFilter(InputFilterInterface $inputFilter)
     {
         throw new \Exception("Not used");    
     }
 
+    // implementsすることでInputFilterAwareInterface内にあるgetInputFilterの実装が義務となる
     public function getInputFilter()
     {
         if (!$this->inputFilter)
@@ -56,7 +62,7 @@ class User
                 'name' => 'name',
                 'required' => false,
                 'filters' => array(
-                    array('name' => 'Int'),
+                    array('name' => 'StripTags'),
                     array('name' => 'StringTrim'),
                 ),
                 'validators' => array(
